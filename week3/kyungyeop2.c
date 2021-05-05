4c4dbad5f6bf10378939d4eb5eb5583575a27f20
KimKyungYeop <81094980+KimKyungYeop@users.noreply.github.com>
2021-05-06 01:39:49 +0900

    Create kyungyeop2.c

---
+++ week3/kyungyeop2.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	
+	int a, b;
+	char c;
+
+	printf("식을 입력하세요(ex. 7+4) : \n");
+	scanf("%d %c %d", &a, &c, &b);
+
+		if (c == '+')
+			printf("%d", a + b);
+
+		else if (c == ' -')
+			printf("%d", a - b);
+
+		else if (c == '*')
+			printf("%d", a * b);
+
+		else if (c == '/')
+			printf("%d", a / b);
+		
+		return 0; 
+}
