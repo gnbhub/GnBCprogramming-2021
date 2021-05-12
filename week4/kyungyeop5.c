87101e50a5d2db9ac519e20998dbbef8088e2256
KimKyungYeop <81094980+KimKyungYeop@users.noreply.github.com>
2021-05-12 18:58:49 +0900

    Create kyungyeop5.c

---
+++ week4/kyungyeop5.c
@@ -0,0 +1,15 @@
+//약수 출력하기
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("a : ");
+    scanf("%d", &a);
+    
+    for (int i=1;i<=a;i++){
+        if (a%i==0)
+        printf("%d\n", i);
+    }
+return 0;
+
+}
