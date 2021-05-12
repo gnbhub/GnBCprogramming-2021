2d25be80ca42de066dcaa1f74e8cfcb54a87ac6d
qkrwldn0 <81032690+qkrwldn0@users.noreply.github.com>
2021-05-12 18:10:06 +0900

    Add files via upload

---
+++ week4/jiwoo4.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	int i, sum = 0;
+	printf("1~10까지 출력하기\n");
+	for (i = 1; i <= 10; i++)
+		printf("%d ", i);
+
+
+
+	return 0;
+
+}
