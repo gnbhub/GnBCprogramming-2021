b4da8d8e930a450f0828929b943e9cf308cbe9c3
qkrwldn0 <81032690+qkrwldn0@users.noreply.github.com>
2021-05-12 18:16:53 +0900

    Add files via upload

---
+++ week4/jiwoo2-5.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+    int i, j = 1;
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%2d의 약수는 ", i);
+        for (j = 1; j <= i; j++)
+        {
+
+            if (i % j == 0)
+                printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}
