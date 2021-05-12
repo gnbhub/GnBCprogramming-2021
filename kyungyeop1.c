de5198126ed023f89257bc9a5508f24e33df0cc9
KimKyungYeop <81094980+KimKyungYeop@users.noreply.github.com>
2021-05-12 18:07:22 +0900

    Create kyungyeop1.c

---
+++ kyungyeop1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int num, a = 1, i;
+    printf("양의 정수를 입력해 주세요 : ");
+    scanf("%d", &num);
+    for(i = 1; i <= num; i++)
+	{
+		a = a * i;
+	}
+    printf("%d! = %d\n", num, a);
+
+return 0;
+}
+
+
+
+//->overflow: 17일때 발생
