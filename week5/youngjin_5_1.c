3c3e848443208677f7592b0371a65b74bf291c77
N-youngjin <81066580+N-youngjin@users.noreply.github.com>
2021-05-17 02:07:21 +0900

    Create youngjin_5_1.c

---
+++ week5/youngjin_5_1.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void) 
+{
+	int a, b, c;
+	int d=1;
+
+	printf("정수 1 : ");
+	scanf("%d", &a);
+	printf("정수 2 : ");
+	scanf("%d", &b);
+
+	if (a < b) {
+		for (c = a; c > 1; c--) {
+			if (a % c == 0 && b % c == 0) {
+				d = d*c;
+				a = a / c;
+				b = b / c;
+			}
+		}
+	}
+	else if (a > b) {
+		for (c = b; c > 1; c--) {
+			if (a % c == 0 && b % c == 0) {
+				d = d*c;
+				a = a / c;
+				b = b / c;
+			}
+		}
+	}
+	else if (a = b){
+		d = b;
+	}
+	printf("최대공약수는 : %d입니다.", d);
+	return 0;
+} // homework 유클리드 알고리즘 설명 부분 이해 안감
