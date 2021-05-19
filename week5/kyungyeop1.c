747a4edc9aa04e3b1984722fdc0ecf7dfc82a229
KimKyungYeop <81094980+KimKyungYeop@users.noreply.github.com>
2021-05-20 00:23:41 +0900

    Create kyungyeop1.c

---
+++ week5/kyungyeop1.c
@@ -0,0 +1,35 @@
+숫자 피라미드 출력하기
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{    int a;
+    int b, c;
+    for (a = 11; a > 10;)
+    {
+        printf("라인 수 : ");
+        scanf("%d", &a);
+        if (a > 10) {
+            printf("[입력오류]피라미드 라인 수는 10이하의 양의 정수만 가능합니다. 다시 입력해주세요. \n");
+        }
+    }
+    if (a <= 10)
+    {
+        for (int b = 0; b < a; b++)
+        {
+            for (int c = 0; c < a - b; c++)
+            {
+                printf(" ");
+            }
+            for (int c = 0; c<= b; c++)
+            {
+                printf("%d", b-c);
+            }
+            for (int c = 0; c<b; c++)
+            {
+                printf("%d", c+1);
+            }
+            printf("\n");
+        }
+    }
+    return 0;
+}
